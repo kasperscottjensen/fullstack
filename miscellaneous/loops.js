b4f1343377464c0ac73f9c84cc1bbd01fbdfe94a
Kasper Scott Jensen <kasp324f@stud.kea.dk>
2023-02-17 09:57:52 +0100

    initial commit

---
+++ miscellaneous/loops.js
@@ -0,0 +1,25 @@
+// rocks
+
+const rocks = [
+    {name: 'led zeppelin', age: 70},
+    {name: 'granite', age: 500_000_000_000},
+    {name: 'dwayne johnson', age: 47}
+];
+
+// map
+
+const rocksAgedOneYear = rocks.map(rock => {
+    rock.age++;
+    return rock;
+});
+
+console.log(rocks);
+console.log(rocksAgedOneYear);
+
+// filter
+
+const rocksEvenAged = rocks.filter(rock => {
+    return rock.age % 2 === 0;
+});
+
+console.log(rocksEvenAged);
